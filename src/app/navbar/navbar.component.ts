4c9de39ebc23e4e73226594fc227cd6d626f1465
racyriaz <racyriaz@gmail.com>
2021-05-29 20:52:43 +0530

    reponsive nav with media queries

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+// import Rlogo from '../../assets/svg/R-logo.svg';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
